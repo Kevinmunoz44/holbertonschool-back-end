1e4c6d344a3069c1101a7ecb7d7220d9481aba64
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-22 12:56:24 -0500

    Update file 0 of the API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,14 @@
 #!/usr/bin/python3
-'''Import the module urllib'''
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-def get_employee_list(employee_id):
-    url = ('https://jsonplaceholder.typecode.com/todos/userId={}'
-           .format(employee_id))
-    response = requests.get(url)
-    then = response.json()
-
-    if not then:
-        print('Employee not found or no tasks available')
-        return
-
-    complete_tasks = [task for task in then if task['completed']]
-    TOTAL_NUMBER_OF_TASKS = len(then)
-
-    EMPLOYEE_NAME = then[0]['userId']
-    NUMBER_OF_DONE_TASKS = len(complete_tasks)
-
-    print('Employee {} is done with tasks {}/{}}'.
-          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-
-    for task in complete_tasks:
-        print(f'\t{task["title"]}')
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python script_name.py employee_id')
-
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_list(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
